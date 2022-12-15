932052433ff8d1547947889c1fed3d0a675a3b3f
FamiliaEn <102762992+FamiliaEn@users.noreply.github.com>
2022-12-15 00:11:27 -0600

    campeon quiniela
    campeon quiniela

--- router.js
+++ router.js
@@ -93,7 +93,12 @@ router.get('/quinielas_data', (req, res)=>{
     conexion.query('SELECT Q.Id, concat_ws(P.Descripcion, P.Id,  P.Id)  Id_P ,R.Alias,L.clave ClaveL,P.Local,Q.ML,Q.MV,P.Visitante,V.Clave ClaveV   \
     FROM partidos as P, paises as L, paises as V, quiniela as Q, participantes R, folder F    \
     WHERE P.Visitante = V.nombre and P.Local = L.nombre and P.Id = Q.Id_partido \
-    and Q.Id_participante=R.Id_participante and Q.Id_participante=F.Id_participante and F.folder = ? order by Id_P',[globalfolder],(error, results)=>{
+    and Q.Id_participante=R.Id_participante and Q.Id_participante=F.Id_participante and F.folder = ? \
+    union all \
+    SELECT Q.Id,concat_ws(P.Descripcion, P.Id,  P.Id)  Id_P ,R.Alias,Q.clave ClaveL,P.Local,null,null,Q.equipo,V.Clave ClaveV \
+    FROM partidos as P, paises as V, campeon as Q, participantes R, folder F \
+    WHERE P.Id > 48 and Q.equipo = V.nombre and P.Id = Q.Id_partido and Q.Id_participante=R.Id_participante and Q.Id_participante=F.Id_participante and F.folder = ? \
+    order by 1,2',[globalfolder,globalfolder],(error, results)=>{
         if(error){
             throw error;
         } else {                                                   
